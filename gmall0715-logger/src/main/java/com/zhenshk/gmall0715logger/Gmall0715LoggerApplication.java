8315f9c5442922984561a4996cc189242b6915d4
甄帅锟 <zhenshk@zhenshuaikundeMacBook-Pro.local>
2019-12-13 16:47:42 +0800

    init project

---
+++ gmall0715-logger/src/main/java/com/zhenshk/gmall0715logger/Gmall0715LoggerApplication.java
@@ -0,0 +1,13 @@
+package com.zhenshk.gmall0715logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0715LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0715LoggerApplication.class, args);
+    }
+
+}
---
+++ gmall0715-logger/src/test/java/com/zhenshk/gmall0715logger/Gmall0715LoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhenshk.gmall0715logger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gmall0715LoggerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
